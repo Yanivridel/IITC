4d67ed5409ed804696ab0c77fef20e0a105e81cc
YanivRidel <117529227+Yanivridel@users.noreply.github.com>
2024-07-28 08:48:43 +0300

    Added Task1

--- 07-25-24/Homework/Task 1/main.js
+++ 07-25-24/Homework/Task 1/main.js
@@ -1,6 +1,28 @@
-console.log("Hello World");
 
-// 1.
-let firstName = "Lirone";
-console.log(firstName);
+let firstName = "Yaniv";
+let _age = 24;
+let isRaining = false, isStudent=true, isLoggedIn =false;
+const PI = 3.14159, DAYS_IN_WEEK=7, COMPANY_NAME="Tech Solutions Inc.";
+let favoriteColor;
+let temperature= 72.5, currentYear=2024;
+let username= "coder123";
+let score = 0, price =19.99, quantity =100, counter =0;
+let message="Hello";
+let myVariable =42;
+myVariable="forty-two";
+let totalCost =50;
+totalCost+=10;
+let username1 = "user1";
+username1="admin";
+let temperature1 =20;
+temperature1 = temperature1*1.8 + 32
+let fruits= ["apple", "banana"];
+let object = {name:firstName,age:_age};
+console.log(object.name);
+let count=5;
+count--;
+let greeting ="Good";
+greeting+= " Morning";
+console.log(greeting);
 
+counter++;
