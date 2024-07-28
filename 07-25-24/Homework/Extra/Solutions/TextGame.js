59a7962208a37b79d64cf8a0a3b7f739893a01c8
YanivRidel <117529227+Yanivridel@users.noreply.github.com>
2024-07-28 21:51:55 +0300

    Completed TextGameSol

--- 07-25-24/Homework/Extra/Solutions/TextGame.js
+++ 07-25-24/Homework/Extra/Solutions/TextGame.js
@@ -1,8 +1,24 @@
 // **** TASK 4: TEXT ADVENTURE GAME **** //
 
-let currentRoom,score,health,random,decision;
+let currentRoom,score,health,random,decision,i;
 const name= setInfo();
-let inventory=[]; //stick,sword,shoes,armor,shield,crossbow,helmet
+let inventory=[];
+
+GameStart();
+
+function GameStart(){
+    let scenarios = [room1DarkForest,room2AbandonedVillage,room3EnchantedLake,room4DragonLair];
+
+    for(i=0;i< scenarios.length;i++){
+        scenarios[i]();
+        if(hadLost(health)){
+            console.log("Game Over ! No more lives");
+            break;
+        }
+    }
+    if(i===scenarios.length)
+        console.log("You won the game! Congrats");
+}
 
 function setInfo(){
     health=100;
@@ -16,7 +32,7 @@ function room1DarkForest(){
         The forest is eerily quiet,
         with the occasional rustle of leaves or
         distant howl breaking the silence.`);
-    decision = confirm("You see a torch at the entrance, do you take it?")
+    decision = confirm("You see a Torch at the entrance, do you take it?")
     if(decision) inventory.push("torch");
     do{
         decision = prompt(`Suddenly you notice a huge tree monster with glowing red eyes, what are you doing?
@@ -24,7 +40,7 @@ function room1DarkForest(){
             2. Hide behind the big bushes.
             3. Fight: use the torch to lit him in fire.`)
         if(decision!==null) decision = Number(decision);
-    }while(1 < decision && decision > 3);
+    }while(isNaN(decision) && 1 < decision && decision > 3);
     switch(decision){
         case 1:
             console.log(`${name}, you manage to escape unharmed!\nCurrent health: ${health}`);
@@ -50,39 +66,121 @@ function room2AbandonedVillage(){
     alert(`The Abandoned Village\nThe houses are in ruins,
         with broken windows and doors hanging off their hinges.
         The air is thick with the smell of decay.`)
+    do{
+        decision = prompt(`Suddenly you notice A band of skeletal warriors with swords, what are you doing?
+            1. Run Away: flee to a nearby building.
+            2. Distract: throw the torch away to distract the skeletons.
+            3. Fight: kick their asses with your fists`)
+        if(decision!==null) decision = Number(decision);
+    }while(isNaN(decision) && 1 < decision && decision > 3);
+    switch(decision){
+        case 1:
+            random = getRandom(1,10);
+            health-=random;
+            console.log(`${name}, you manage to escape but 1 skelton found you! you lost ${random} health.\nCurrent health: ${health}`);
+            break;
+        case 2:
+            if(inventory.includes("torch")){
+                console.log(`${name}, you lost the torch but managed to distract the skeletons!\nCurrent health: ${health}`);
+                removeFromArray("torch");
+                decision = confirm("You found an Abandoned Sword, do you take it?")
+                if (decision) inventory.push("abandoned sword");
+            }else{
+                random = getRandom(30,50);
+                health-=random;
+                console.log(`You don't even have a torch remember?\nyou lost ${random} health.\nCurrent health: ${health}`);
+            }
+            break;
+        case 3:
+            random = getRandom(30,50);
+            health-=random;
+            console.log(`${name}, you really thought you could overcome 40 skeletons alone?\nyou lost ${random} health.\nCurrent health: ${health}`);
+            break;
+    }
+}
+function room3EnchantedLake(){
+    alert(`The Enchanted Lake\nThe water is crystal clear,
+         and the surroundings are lush and green.
+         There's a mystical aura in the air,
+         and the lake's surface glows faintly under the moonlight.`)
         do{
-            decision = prompt(`Suddenly you notice A band of skeletal warriors with swords, what are you doing?
-                1. Run Away: flee to a nearby building.
-                2. Distract: throw the torch away to distract the skeletons.
-                3. Fight: kick their asses with your fists`)
+            decision = prompt(`A water nymph with control over the lake's waters shown, what are you doing?
+                1. Run Away: Swim back to the shore and escape.
+                2. Appease: offer your sword as a gift to the monster.
+                3. Fight: use the sword to kill the nymph`)
             if(decision!==null) decision = Number(decision);
-        }while(1 < decision && decision > 3);
+        }while(isNaN(decision) && 1 < decision && decision > 3);
         switch(decision){
             case 1:
-                random = getRandom(1,10);
+                random = getRandom(40,60);
                 health-=random;
-                console.log(`${name}, you manage to escape but 1 skelton found you! you lost ${random} health.\nCurrent health: ${health}`);
+                console.log(`${name}, the nymph control the water, theres no escape! you lost ${random} health.\nCurrent health: ${health}`);
                 break;
             case 2:
-                console.log(`${name}, you lost your torch but you managed to escape!\nCurrent health: ${health}`);
+                if(inventory.includes("abandoned sword")){
+                    console.log(`${name}, the nymph liked the sword and let you pass!\nCurrent health: ${health}`);
+                    removeFromArray("abandoned sword");
+                    decision = confirm("The nymph offers you an healing potion, do you take it?")
+                    if (decision) {
+                        health+=40;
+                        console.log(`You received 40hp\nCurrent health: ${health}`);
+                    }
+                }
+                else{
+                    random = getRandom(30,40);
+                    health-=random;
+                    console.log(`You didn't take the sword remember?, you lost ${random} health.\nCurrent health: ${health}`);
+                }
                 break;
             case 3:
-                let check= inventory.includes("torch");
-                if(check){
-                    console.log(`${name}, you manage to distract the skeletons!\nCurrent health: ${health}`);
-                    decision = confirm("You found an Abandoned Sword, do you take it?")
-                    if (decision) inventory.push("abandoned sword");
-                }else{
-                    random = getRandom(30,50);
+                if(inventory.includes("abandoned sword")){
+                    random = getRandom(30,40);
                     health-=random;
-                    console.log(`${name}, you really thought you can overcome 40 skeletons alone?\nyou lost ${random} health.\nCurrent health: ${health}`);
+                    console.log(`${name}, sword does nothing to water.. you lost ${random} health.\nCurrent health: ${health}`);
+                }
+                else{
+                    random = getRandom(30,40);
+                    health-=random;
+                    console.log(`You didn't take the sword remember?, you lost ${random} health.\nCurrent health: ${health}`);
                 }
                 break;
         }
 }
-
-room1DarkForest();
-room2AbandonedVillage();
+function room4DragonLair(){
+    alert(`The Dragon's Lair\nThe air is hot and filled with the smell of sulfur.
+        The ground is uneven and covered in ash.
+        The dragon is a massive, fearsome creature with scales like armor 
+        and eyes that glow with an inner fire.`)
+    do{
+        decision = prompt(`The final Dragon boss lies here, what are you doing?
+            1. Hide: Find a large rock or hollow tree to hide behind and wait for the monster to pass.
+            2. Negotiate: attempt to communicate with the dragon and offer a trade or plea for mercy.
+            3. Fight: use the sword to engage the dragon in combat.`)
+        if(decision!==null) decision = Number(decision);
+    }while(isNaN(decision) && 1 < decision && decision > 3);
+    switch(decision){
+        case 1:
+            random = getRandom(30,40);
+            health-=random;
+            console.log(`${name}, its too hot there, your burning alive! you lost ${random} health.\nCurrent health: ${health}`);
+            break;
+        case 2:
+            random = getRandom(30,40);
+            health-=random;
+            console.log(`${name}, you do not make deals with the devil.. you lost ${random} health.\nCurrent health: ${health}`);
+            break;
+        case 3:
+            if(inventory.includes("abandoned sword")){
+                console.log(`${name}, the battle was hard but you won!\nCurrent health: ${health}`);
+                decision = confirm("You see the dragons egg, do you take it?")
+                if (decision) inventory.push("dragon egg");
+            }else{
+                random = getRandom(30,50);
+                health-=random;
+                console.log(`You don't even have a sword remember?\nyou lost ${random} health.\nCurrent health: ${health}`);
+            }
+    }
+}
 
 
 function hadLost(health){
@@ -92,7 +190,7 @@ function getRandom(min,max){
     return random = Math.floor(Math.random() * (max-min+1) + min);
 }
 function removeFromArray(element){
-    let index = array.indexOf(element);
+    let index = inventory.indexOf(element);
     if (index > -1)
-        array.splice(index, 1); //from index remove 1 item.
+        inventory.splice(index, 1); //from index remove 1 item.
 }
