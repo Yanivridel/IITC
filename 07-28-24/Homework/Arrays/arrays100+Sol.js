c86aae072a5b31c69875b6b801c859fe3eca80c8
YanivRidel <117529227+Yanivridel@users.noreply.github.com>
2024-07-29 16:11:46 +0300

    50/100

--- 07-28-24/Homework/Arrays/arrays100+Sol.js
+++ 07-28-24/Homework/Arrays/arrays100+Sol.js
@@ -1,10 +1,5 @@
 
 let i=0;
-function printArr(arr){
-    for(let i=0;i<arr.length;i++){
-        console.log(arr[i]);
-    }
-}
 //1
 let fruits=[];
 //2
@@ -50,7 +45,7 @@ console.log(fruits[15]);
 console.log("Task 15");
 console.log(`The second color is ${colors[1]}`);
 //16
-fruits[0] = "apple";
+fruits[0] = "Apple";
 //17
 numbers[numbers.length-1] = 10;
 //1
@@ -60,106 +55,167 @@ console.log("Task 19");
 for(i=0;i<colors.length;i++){
     colors[i] = colors[i].toUpperCase()
 }
-printArr(colors);
+console.log(colors.toString());
 //20
 console.log("Task 20");
 let temp = seasons[0];
 seasons[0] = seasons[seasons.length-1];
 seasons[seasons.length-1] = temp;
-printArr(seasons);
+console.log(seasons.toString());
 //21
 console.log("Task 21");
-
+console.log(fruits.toString());
+fruits.push("Orange");
+console.log(fruits.toString());
 //22
 console.log("Task 22");
-
+console.log(numbers.toString());
+numbers.pop();
+console.log(numbers.toString());
 //23
 console.log("Task 23");
-
+console.log(colors.toString());
+colors.push("Yellow","Pink","Purple");
+console.log(colors.toString());
 //24
 console.log("Task 24");
-
+let empty = []
+for(i=1;i<=5;i++){
+    empty.push(i);
+}
+console.log(empty.toString());
 //25
 console.log("Task 25");
-/*
+while(empty.length>0){
+    console.log(empty.pop());
+}
+console.log(empty.toString());
 //26
 console.log("Task 26");
-
+console.log(fruits.toString());
+fruits.unshift("Mango");
+console.log(fruits.toString());
 //27
 console.log("Task 27");
-
+console.log(numbers.toString());
+numbers.shift();
+console.log(numbers.toString());
 //28
 console.log("Task 28");
-
+console.log(numbers.toString());
+numbers.unshift(1,7,9);
+console.log(numbers.toString());
 //29
 console.log("Task 29");
-
+empty = []
+for(i=1;i<=5;i++){
+    empty.unshift(i);
+}
+console.log(empty.toString());
 //30
 console.log("Task 30");
-
+for(i=1;i<=5;i++){
+    console.log(empty.shift());
+}
+console.log(empty.toString());
 //31
 console.log("Task 31");
-
+console.log(colors.indexOf("GREEN"));
 //32
 console.log("Task 32");
-
+console.log(seasons.indexOf("winter")!==-1); 
 //33
 console.log("Task 33");
-
+console.log(colors.toString());
+colors.push("red");
+console.log(colors.toString());
+console.log(colors.lastIndexOf("red"));
 //34
 console.log("Task 34");
-
+let colorsDup =[]
+for(i=0;i<colors.length;i++){
+    colorsDup.push(colors[i],colors[i]);
+}
+console.log(colorsDup.toString());
+console.log(`first green: ${colorsDup.indexOf("GREEN")}
+last green: ${colorsDup.lastIndexOf("GREEN")}`);
 //35
 console.log("Task 35");
-
+console.log(`Does numbers have 7? answer: ${numbers.indexOf(7)!==-1}`);
 //36
 console.log("Task 36");
-
+console.log(fruits.includes("Apple"));
 //37
 console.log("Task 37");
-
+console.log(numbers.includes(10));
 //38
 console.log("Task 38");
-
+console.log(colors.toString());
+console.log(colors.includes("BLUE",Math.floor(colors.length/2)));
 //39
 console.log("Task 39");
-
+function includeArr(arr,val){
+    return arr.includes(val);
+}
 //40
 console.log("Task 40");
-
+console.log("Searching Mango in fruits:");
+console.log(includeArr(fruits,"Mango") ? "Found it!":"Where is it?");
 //41
 console.log("Task 41");
-
+console.log(numbers.toString());
+console.log(numbers.slice(3).toString());
 //42
 console.log("Task 42");
-
+console.log(colors.toString());
+console.log(colors.slice(-2).toString());
 //43
 console.log("Task 43");
-
+console.log(seasons.toString());
+console.log(seasons.slice(1,3).toString());
 //44
 console.log("Task 44");
-
+let copyNumbers = numbers.slice();
+console.log(numbers.toString());
+console.log(copyNumbers.toString());
 //45
 console.log("Task 45");
-
+let idx = Math.floor(numbers.length/2);
+console.log(numbers.toString());
+console.log(numbers.slice(idx,idx+1).toString());
 //46
 console.log("Task 46");
+idx = Math.floor(fruits.length/2);
+console.log(fruits.toString());
+fruits.splice(idx,2).toString()
+console.log(fruits.toString());
+// array.splice(index, count, item1, ....., itemX)
 
 //47
 console.log("Task 47");
+console.log(numbers.toString());
+numbers.splice(0,1,37)
+console.log(numbers.toString());
 
 //48
 console.log("Task 48");
-
+console.log(colors.toString());
+colors.splice(2,0,"Magenta","Gray")
+console.log(colors.toString());
 //49
 console.log("Task 49");
-
+console.log(colors.toString());
+colors.splice(0,1,"DarkBlue","Gold")
+console.log(colors.toString());
 //50
 console.log("Task 50");
-
+empty.push(1,2,3,4,5);
+console.log(empty.toString());
+empty.splice(0,empty.length)
+console.log(empty.toString());
 //51
 console.log("Task 51");
-
+/*
 //52
 console.log("Task 52");
 
